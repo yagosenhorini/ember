d775658943e78ec2f02f77c6989423959fd0b878
yagosenhorini <yagosenhorini30@gmail.com>
2017-12-06 17:13:40 -0200

    Sass cli and mirage include

---
+++ mirage/config.js
@@ -0,0 +1,20 @@
+export default function() {
+    this.namespace = '/api';
+    this.get('/rentals', function() {
+        return {
+            data: [{
+                type: 'rentals',
+                id: 'grand-old-mansion',
+                attributes: {
+                    title: 'Grand Old Mansion',
+                    owner: 'Veruca Salt',
+                    city: 'San Francisco',
+                    "property-type": 'Estate',
+                    bedrooms: 15,
+                    image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+
+                }
+            }]
+        };
+    });
+}
